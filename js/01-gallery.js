8aea2e3f1fc5f7bc615d0ad40bc866ffbc3db8c3
Bordiuzhan <bordyujan@gmail.com>
2022-09-20 12:17:56 +0200

    created  a marks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEL = document.querySelector('.gallery');
+
+const elementsOfGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join(' ');
+
+galleryContainerEL.insertAdjacentHTML('beforeend', elementsOfGallery);
+
+galleryContainerEL.addEventListener('click', onBigImg);
+
+function onBigImg(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.dir(e.target.dataset.source);
+}
+const instance = basiclightbox.create(`
+	
+`);
